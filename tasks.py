d99e6d8552807f92691488bfcb7a0495b2da5269
Calvin Remsburg <cremsburg.dev@gmail.com>
2022-07-13 20:12:05 +0000

    refine invoke to pull from local environmentals

--- tasks.py
+++ tasks.py
@@ -1,89 +1,127 @@
-"""Tasks for use with Invoke.
-
-(c) 2021 Calvin Remsburg
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-  http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-"""
+"""Tasks for use with Invoke."""
 
 import os
+import logging
 from invoke import task
 
-### ---------------------------------------------------------------------------
-### DOCKER PARAMETERS
-### ---------------------------------------------------------------------------
-DOCKER_IMG = "registry.gitlab.com/cremsburg/juniper-automation-container"
-DOCKER_TAG = "deploy-vsrx"
+# ---------------------------------------------------------------------------
+# DOCKER PARAMETERS
+# ---------------------------------------------------------------------------
+DOCKER_IMG = "ghcr.io/cdot65/deploy-vsrx-on-vmware"
+DOCKER_TAG = "0.0.1"
 
-### ---------------------------------------------------------------------------
-### SYSTEM PARAMETERS
-### ---------------------------------------------------------------------------
+# ---------------------------------------------------------------------------
+# SYSTEM PARAMETERS
+# ---------------------------------------------------------------------------
 PWD = os.getcwd()
 
-### ---------------------------------------------------------------------------
-### VCENTER PARAMETERS
-### ---------------------------------------------------------------------------
-VCENTER_HOSTNAME='vcenter.dmz.home'
-VCENTER_USERNAME='ansible@dmz.home'
-VCENTER_PASSWORD='ansible123'
-DATACENTER='home'
-FOLDER='/NetworkVirtualization'
-TEMPLATE='vsrx3_21.x'
-ESXI_HOST='esx5.dmz.home'
-VM_NAME='ansible-test'
+# ---------------------------------------------------------------------------
+# ANSIBLE ARGUMENTS AND COMMANDS
+# ---------------------------------------------------------------------------
+VCENTER_HOSTNAME = os.environ.get("VCENTER_HOSTNAME", "vcenter.dmz.home")
+VCENTER_USERNAME = os.environ.get("VCENTER_USERNAME", "ansible")
+VCENTER_PASSWORD = os.environ.get("VCENTER_PASSWORD", "juniper123")
+VCENTER_DATACENTER = os.environ.get("VCENTER_DATACENTER", "Datacenter")
+VCENTER_FOLDER = os.environ.get("VCENTER_FOLDER", "/Templates")
+VCENTER_TEMPLATE = os.environ.get("VCENTER_TEMPLATE", "vsrx-template")
+VCENTER_ESXI_HOST = os.environ.get("VCENTER_ESXI_HOST", "dell-r820.dmz.home")
 
-### ---------------------------------------------------------------------------
-### DOCKER CONTAINER BUILD
-### ---------------------------------------------------------------------------
-@task
-def container(context):
-    # Build our docker image
+VM_NAME = "ansible-test"
+
+
+# ---------------------------------------------------------------------------
+# LOGGING PARAMETERS
+# ---------------------------------------------------------------------------
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+console_handler = logging.StreamHandler()
+log_format = "%(asctime)s | %(levelname)s: %(message)s"
+console_handler.setFormatter(logging.Formatter(log_format))
+logger.addHandler(console_handler)
+
+
+# ---------------------------------------------------------------------------
+# HELPER FUNCTIONS
+# ---------------------------------------------------------------------------
+def console_msg(message):
+    """Provide a little formatting help for console messages."""
+    logger.info(message)
+
+
+def run_command(context, command, **kwargs):
+    """Helper function to run commands based on arguments."""
+    context.run(command, **kwargs)
+
+
+# ---------------------------------------------------------------------------
+# ANSIBLE CONTAINER IMAGE BUILD
+# ---------------------------------------------------------------------------
+@task(
+    help={
+        "force_rm": "Always remove existing containers.",
+        "cache": "Determine whether or not to use local cache.",
+    }
+)
+def build(context, force_rm=False, cache=True):
+    """Build our Ansible docker container image.
+    Args:
+        context (obj): Used to run specific commands
+        force_rm (Bool): will remove any local instance [default: False]
+        cache (Bool): determine whether or not to use cache [default: True]
+    """
+
+    # build command pointing to a folder outside our local context
+    command = "docker build -f docker/Dockerfile"
+
+    if not cache:
+        command += " --no-cache"
+    if force_rm:
+        command += " --force-rm"
+
+    # tokens used by our app are passed into the container build process
+    # ansible_username = f"ANSIBLE_NET_USERNAME={VCENTER_USERNAME}"
+    # ansible_password = f"ANSIBLE_NET_PASSWORD={VCENTER_PASSWORD}"
+
+    # build arguments pass our tokens into the build process
+    # ansible_args = f"--build-arg={ansible_username} --build-arg={ansible_password}"
+
+    console_msg(f"Building our Docker container image {DOCKER_IMG}:{DOCKER_TAG}")
     context.run(
-        f"docker build -t {DOCKER_IMG}:{DOCKER_TAG} files/docker/",
+        f"{command} -t {DOCKER_IMG}:{DOCKER_TAG} .",
     )
 
-### ---------------------------------------------------------------------------
-### DOCKER CONTAINER SHELL
-### ---------------------------------------------------------------------------
+
+# ---------------------------------------------------------------------------
+# DOCKER CONTAINER SHELL
+# ---------------------------------------------------------------------------
 @task
 def shell(context):
     # Get access to the BASH shell within our container
     print("Jumping into container, type exit to return to host")
     context.run(
         f"docker run -it --rm \
-            -v {PWD}/files/:/home/tmp/files \
-            -v {PWD}/files/:/home/tmp/files \
-            -w /home/tmp/files/ansible/ \
-            {DOCKER_IMG}:{DOCKER_TAG} /bin/bash",
+            {DOCKER_IMG}:{DOCKER_TAG} /bin/sh",
         pty=True,
     )
 
 
-### ---------------------------------------------------------------------------
-### EXECUTE PLAYBOOK FROM WITHIN CONTAINER
-### ---------------------------------------------------------------------------
+# ---------------------------------------------------------------------------
+# EXECUTE PLAYBOOK FROM WITHIN CONTAINER
+# ---------------------------------------------------------------------------
 @task
 def ansible(context):
     # Execute Ansible playbook from within the container
     context.run(
         f"docker run -it \
             --rm \
-            -v {PWD}/files/ansible/:/home/ansible \
-            -w /home/ansible/ \
-            {DOCKER_IMG}:{DOCKER_TAG} ansible-playbook -vvv pb.deploy.vsrx.yaml \
+            {DOCKER_IMG}:{DOCKER_TAG} ansible-playbook -vvv deploy.vsrx.yaml \
             -e vcenter_hostname='{VCENTER_HOSTNAME}' \
             -e vcenter_username='{VCENTER_USERNAME}' \
             -e vcenter_password='{VCENTER_PASSWORD}' \
-            -e datacenter='{DATACENTER}' \
-            -e folder='{FOLDER}' \
-            -e template='{TEMPLATE}' \
-            -e esxi_host='{ESXI_HOST}' \
+            -e datacenter='{VCENTER_DATACENTER}' \
+            -e folder='{VCENTER_FOLDER}' \
+            -e template='{VCENTER_TEMPLATE}' \
+            -e esxi_host='{VCENTER_ESXI_HOST}' \
             -e vm_name='{VM_NAME}'",
         pty=True,
     )
